b2553e167eb0fc74d810429c9f10ddc8b1287b0d
Gokul <gokulchandan24@gmail.com>
2025-05-31 01:31:32 +0530

    shadcn installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const Page = async () => {
   return (
     <main>
       <h1>Popular Companions</h1>
+      <Button>Close</Button>
     </main>
   );
 };
