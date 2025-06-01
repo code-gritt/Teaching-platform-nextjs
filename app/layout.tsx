cbc73282fa91061012d486c29c1125eea9e59a75
Gokul <gokulchandan24@gmail.com>
2025-06-01 08:25:49 +0530

    created basic components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const bricolage = Bricolage_Grotesque({
   variable: "--font-bricolage",
@@ -20,10 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${bricolage.variable} antialiased`}>
-        {/* <ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}> */}
-        {/* <Navbar /> */}
-        {children}
-        {/* </ClerkProvider> */}
+        <ClerkProvider appearance={{ variables: { colorPrimary: "#fe5933" } }}>
+          <Navbar />
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   );
