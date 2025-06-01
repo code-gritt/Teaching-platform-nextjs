9ac4e6bab7944b06b76a102a08f7422be8775590
Gokul <gokulchandan24@gmail.com>
2025-06-01 12:40:20 +0530

    integrated clerk

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,14 +1,24 @@
+import CompanionForm from "@/components/CompanionForm";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 
 const NewCompanion = async () => {
+  const { userId } = await auth();
+  if (!userId) redirect("/sign-in");
+
+  // const canCreateCompanion = await newCompanionPermissions();
+
   return (
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      {/* {canCreateCompanion ? ( */}
       <article className="w-full gap-4 flex flex-col">
         <h1>Companion Builder</h1>
-      </article>
 
+        <CompanionForm />
+      </article>
+      {/* ) : ( */}
       <article className="companion-limit">
         <Image
           src="/images/limit.svg"
@@ -29,6 +39,7 @@ const NewCompanion = async () => {
           Upgrade My Plan
         </Link>
       </article>
+      {/* )} */}
     </main>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = async () => {
   return (
     <main>
       <h1>Popular Companions</h1>
-      <Button>Close</Button>
+
+      <section className="home-section">
+        <CompanionCard
+          id={"123"}
+          name={"Neura the brainy explorer"}
+          topic={""}
+          subject={""}
+          duration={0}
+          color={""}
+          bookmarked={false}
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
     </main>
   );
 };
