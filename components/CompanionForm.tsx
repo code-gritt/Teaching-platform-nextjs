eadddaf8c93ecc707885738c5c94fd8754b51470
Gokul <gokulchandan24@gmail.com>
2025-06-01 13:27:43 +0530

    integrated supabase

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -23,6 +23,7 @@ import {
 } from "@/components/ui/select";
 import { subjects } from "@/constants";
 import { Textarea } from "@/components/ui/textarea";
+import { createCompanion } from "@/lib/actions/companion.actions";
 import { redirect } from "next/navigation";
 
 const formSchema = z.object({
@@ -47,20 +48,20 @@ const CompanionForm = () => {
     },
   });
 
-  //   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-  //     const companion = await createCompanion(values);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const companion = await createCompanion(values);
 
-  //     if (companion) {
-  //       redirect(`/companions/${companion.id}`);
-  //     } else {
-  //       console.log("Failed to create a companion");
-  //       redirect("/");
-  //     }
-  //   };
+    if (companion) {
+      redirect(`/companions/${companion.id}`);
+    } else {
+      console.log("Failed to create a companion");
+      redirect("/");
+    }
+  };
 
   return (
     <Form {...form}>
-      <form className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
           name="name"
